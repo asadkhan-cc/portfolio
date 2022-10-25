3929819257af226b80e2a7e86ff3714f63f6646c
asadkhan-cc <asad.khan@cooperativecomputing.com>
2022-10-25 19:17:38 +0500

    schema defined in sanity

---
+++ backend-sanity/schemas/skill.js
@@ -0,0 +1,28 @@
+export default {
+  name: "skill",
+  title: "Skill",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of the Skill",
+      type: "string",
+    },
+    {
+      name: "progress",
+      title: "Progres",
+      type: "number",
+      description: "Progress of skill from 0 to 100%",
+      validation: (Rule) => Rule.min(e).max(100),
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
---
+++ backend-sanity/schemas/social.js
@@ -0,0 +1,18 @@
+export default {
+  name: "Social",
+  title: "Social",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "Title of the Skill",
+      type: "string",
+    },
+    {
+      name: "url",
+      title: "Url",
+      type: "url",
+    },
+  ],
+};
